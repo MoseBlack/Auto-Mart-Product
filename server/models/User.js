91563de8b0ebd7043da0a17ad5339c5995fe742a
MoseBlack <moseselearn@gmail.com>
2019-06-08 13:11:21 +0200

    Signup api endpoint

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const Users = [
+  {
+    id: 1,
+    email: 'user_1@gmail.com',
+    first_name: 'Koby',
+    last_name: 'Bryant',
+    password: 'losangeles123',
+    address: 'Lakers',
+    is_admin: true,
+
+  },
+  {
+    id: 2,
+    email: 'user_2@gmail.com',
+    first_name: 'Michael',
+    last_name: 'Jordan',
+    password: 'chicago123',
+    address: 'Chicago',
+    is_admin: true,
+
+  },
+  {
+    id: 3,
+    email: 'user_3@gmail.com',
+    first_name: 'Lionel',
+    last_name: 'Messi',
+    password: 'barca123',
+    address: 'Barcelona',
+    is_admin: true,
+
+  },
+];
+module.exports = Users;
