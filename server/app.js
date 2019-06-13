9711880fcbaaf760f6062ace1143b72cbeb3f569
MoseBlack <moseselearn@gmail.com>
2019-06-13 21:06:35 +0200

    test heroku

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = require('./routes/User');
+// const router = require('./routes/User');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/v1/users', router);
+app.use('/', (req, res) => {
+  res.json('Good');
+});
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
