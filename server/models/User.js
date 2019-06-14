dd3fa93ce2565b7792d3c03ecfc4ca6c5a113a36
MoseBlack <moseselearn@gmail.com>
2019-06-14 05:35:49 +0200

    signup endpoint

--- server/models/User.js
+++ server/models/User.js
@@ -1,36 +1,18 @@
-const Users = [
-  {
-    id: 1,
-    token: '45erkjherht45495783',
-    email: 'user_1@gmail.com',
-    first_name: 'Koby',
-    last_name: 'Bryant',
-    password: 'losangeles123',
-    address: 'Lakers',
-    is_admin: true,
+/* eslint-disable camelcase */
+// const userList = require('../controllers/User');
 
-  },
-  {
-    id: 2,
-    token: '45erkjherht45495784',
-    email: 'user_2@gmail.com',
-    first_name: 'Michael',
-    last_name: 'Jordan',
-    password: 'chicago123',
-    address: 'Chicago',
-    is_admin: true,
+const user = (data, id, is_admin) => {
+  const newUser = {
+    id,
+    firstname: data.firstname,
+    lastname: data.lastname,
+    email: data.email,
+    password: data.password,
+    is_admin,
+  };
+  return newUser;
+};
 
-  },
-  {
-    id: 3,
-    token: '45erkjherht45495785',
-    email: 'user_3@gmail.com',
-    first_name: 'Lionel',
-    last_name: 'Messi',
-    password: 'barca123',
-    address: 'Barcelona',
-    is_admin: true,
-
-  },
-];
-module.exports = Users;
+module.exports = {
+  user_model: user,
+};
